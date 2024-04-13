d0958583a1f2470d4404b84f361a6e36bab9d579
Hansana K.T <it21167850@my.sliit.lk>
2024-04-13 22:45:03 +0530

    create online-educational-platform web application usign nextjs

---
+++ app/(dashboard)/(routes)/teacher/analytics/page.tsx
@@ -0,0 +1,9 @@
+const AnaliticsPage = () => {
+  return (
+    <div>
+      <h1>Analitics Page</h1>
+    </div>
+  );
+};
+
+export default AnaliticsPage;
---
+++ app/(dashboard)/_components/sidebar-routes.tsx
@@ -0,0 +1,51 @@
+"use client";
+
+import { BarChart, Compass, Layout, List } from "lucide-react";
+import { SideBarItem } from "./sidebar-item";
+import { usePathname } from "next/navigation";
+
+const guestRoutes = [
+  {
+    icon: Layout,
+    label: "Dashboard",
+    href: "/",
+  },
+  {
+    icon: Compass,
+    label: "Browse",
+    href: "/search",
+  },
+];
+
+const teeacherRoutes = [
+  {
+    icon: List,
+    label: "Courses",
+    href: "/teacher/courses",
+  },
+  {
+    icon: BarChart,
+    label: "Analitics",
+    href: "/teacher/analytics",
+  },
+];
+
+export const SidebarRoutes = () => {
+  const pathname = usePathname();
+
+  const isTeacherPage = pathname?.includes("/teacher");
+
+  const routes = isTeacherPage ? teeacherRoutes : guestRoutes;
+  return (
+    <div className="flex flex-col w-full">
+      {routes.map((route, index) => (
+        <SideBarItem
+          key={route.href}
+          icon={route.icon}
+          label={route.label}
+          href={route.href}
+        />
+      ))}
+    </div>
+  );
+};
